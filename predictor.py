d53496b0580d31072cd606b24b0684b873ff67c9
poisonshade <imildositoe@gmail.com>
2023-09-08 18:52:55 +0200

    added training data function

--- predictor.py
+++ predictor.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
 
 #load data
 data = pd.read_csv('corona_cases.csv')
@@ -17,10 +18,19 @@ x = np.array(data['id']).reshape(-1, 1)
 y = np.array(data['cases']).reshape(-1, 1)
 #ploting to a  visual graph
 plt.plot(y, '-m')
-plt.show()
+# plt.show()
 
 #adding more powered columns
-polyFeart = PolynomialFeatures(degree=2)
+polyFeart = PolynomialFeatures(degree=1)
 x = polyFeart.fit_transform(x)
 print(x)
 
+#training data
+print('-------------------------TRAINING DATA----------------------')
+model = linear_model.LinearRegression()
+model.fit(x, y)
+accuracy = model.score(x, y)
+print(f'Accuracy:{round(accuracy*100, 3)} %')
+y0 = model.predict(x)
+plt.plot(y0, '--b')
+plt.show()
